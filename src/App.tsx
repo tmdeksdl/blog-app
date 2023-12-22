dcd8da3ed98d88a30d4be384d1153a7c2da3320e
jpark <tmdeksdl@gmail.com>
2023-12-22 20:46:20 +0900

    Feat: 기본 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">PostInfo</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">NewPost</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">Edit</Link>
+        </li>
+        <li>
+          <Link to="/posts/profile">Profile</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/posts" element={<h1>post list page</h1>}></Route>
+        <Route path="/posts/:id" element={<h1>Post Info Page</h1>}></Route>
+        <Route path="/posts/new" element={<h1>New Post</h1>}></Route>
+        <Route path="/posts/edit/:id" element={<h1>Edit Post</h1>}></Route>
+        <Route path="/posts/profile" element={<h1>Profile</h1>}></Route>
+        <Route path="*" element={<Navigate replace to="/" />}></Route>
+      </Routes>
+    </>
   );
 }
 
