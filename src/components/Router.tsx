dc154e262268f0a5761134f00d22d2fd3b26eee5
jpark <tmdeksdl@gmail.com>
2023-12-28 15:55:03 +0900

    Feat: 로그인 여부에 따른 라우팅 설정 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from 'pages/home';
 import PostList from 'pages/posts';
@@ -9,17 +11,29 @@ import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
 export default function Router() {
+  //firebase Auth가 인증되면 true로 변경해주는 로직추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/posts" element={<PostList />}></Route>
-      <Route path="/posts/:id" element={<PostDetail />}></Route>
-      <Route path="/posts/new" element={<PostNew />}></Route>
-      <Route path="/posts/edit/:id" element={<PostEdit />}></Route>
-      <Route path="/profile" element={<ProfilePage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/signup" element={<SignupPage />}></Route>
-      <Route path="*" element={<Navigate replace to="/" />}></Route>
+      {isAuthenticated ? (
+        <>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/posts" element={<PostList />}></Route>
+          <Route path="/posts/:id" element={<PostDetail />}></Route>
+          <Route path="/posts/new" element={<PostNew />}></Route>
+          <Route path="/posts/edit/:id" element={<PostEdit />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="*" element={<Navigate replace to="/" />}></Route>
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="*" element={<LoginPage />}></Route>
+        </>
+      )}
     </Routes>
   );
 }
