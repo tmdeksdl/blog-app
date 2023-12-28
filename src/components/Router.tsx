3895cc674c93a03b4c46416c4805192ea410f4ba
jpark <tmdeksdl@gmail.com>
2023-12-28 17:33:55 +0900

    Feat: Firebase Auth 세팅

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from 'pages/home';
 import PostList from 'pages/posts';
@@ -10,30 +8,34 @@ import ProfilePage from 'pages/profile';
 import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
-export default function Router() {
-  //firebase Auth가 인증되면 true로 변경해주는 로직추가
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
   return (
-    <Routes>
-      {isAuthenticated ? (
-        <>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/posts" element={<PostList />}></Route>
-          <Route path="/posts/:id" element={<PostDetail />}></Route>
-          <Route path="/posts/new" element={<PostNew />}></Route>
-          <Route path="/posts/edit/:id" element={<PostEdit />}></Route>
-          <Route path="/profile" element={<ProfilePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/signup" element={<SignupPage />}></Route>
-          <Route path="*" element={<Navigate replace to="/" />}></Route>
-        </>
-      ) : (
-        <>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/signup" element={<SignupPage />}></Route>
-          <Route path="*" element={<LoginPage />}></Route>
-        </>
-      )}
-    </Routes>
+    <>
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/posts" element={<PostList />}></Route>
+            <Route path="/posts/:id" element={<PostDetail />}></Route>
+            <Route path="/posts/new" element={<PostNew />}></Route>
+            <Route path="/posts/edit/:id" element={<PostEdit />}></Route>
+            <Route path="/profile" element={<ProfilePage />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/signup" element={<SignupPage />}></Route>
+            <Route path="*" element={<Navigate replace to="/" />}></Route>
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/signup" element={<SignupPage />}></Route>
+            <Route path="*" element={<LoginPage />}></Route>
+          </>
+        )}
+      </Routes>
+    </>
   );
 }
