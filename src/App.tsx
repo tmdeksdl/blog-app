4f47f37fa51a5032add1f14be84ffdba32ddc389
jpark <tmdeksdl@gmail.com>
2023-12-28 19:04:17 +0900

    Feat: 회원가입 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { app } from 'firebaseApp';
 import { getAuth } from 'firebase/auth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Router from './components/Router';
 
@@ -10,7 +12,12 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
-  return <Router isAuthenticated={isAuthenticated} />;
+  return (
+    <>
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
+    </>
+  );
 }
 
 export default App;
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,17 +1,83 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { app } from 'firebaseApp';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function SignupForm() {
+  const [error, setError] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth(app);
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      toast.success('회원가입에 성공했습니다.');
+    } catch (error: any) {
+      toast.error(error?.code);
+    }
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'email') {
+      setEmail(value);
+      const validRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+      if (!value?.match(validRegex)) {
+        setError('이메일 형식이 올바르지 않습니다.');
+      } else {
+        setError('');
+      }
+    }
+    if (name === 'password') {
+      setPassword(value);
+      if (value?.length < 8) {
+        setError('비밀번호는 8자리 이상으로 입력해주세요.');
+      } else if (passwordConfirm?.length > 0 && value !== passwordConfirm) {
+        setError('비밀번호와 비밀번호 확인 값이 다릅니다. 다시 확인해주세요.');
+      } else {
+        setError('');
+      }
+    }
+    if (name === 'password_confirm') {
+      setPasswordConfirm(value);
+      if (value?.length < 8) {
+        setError('비밀번호는 8자리 이상으로 입력해주세요');
+      } else if (value !== password) {
+        setError('비밀번호와 값이 다릅니다. 다시 확인해주세요.');
+      } else {
+        setError('');
+      }
+    }
+  };
   return (
     <>
-      <form action="/post" method="post" className="form form--lg">
+      <form onSubmit={onSubmit} className="form form--lg">
         <h1 className="form_title">회원가입</h1>
         <div className="form_block">
           <label htmlFor="email">이메일</label>
-          <input type="text" name="email" id="email" required />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            required
+            onChange={onChange}
+          />
         </div>
         <div className="form_block">
           <label htmlFor="password">비밀번호</label>
-          <input type="text" name="password" id="password" required />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            required
+            onChange={onChange}
+          />
         </div>
 
         <div className="form_block">
@@ -21,8 +87,14 @@ export default function SignupForm() {
             name="password_confirm"
             id="password_confirm"
             required
+            onChange={onChange}
           />
         </div>
+        {error && error?.length > 0 && (
+          <div className="form_block">
+            <div className="form_error">{error}</div>
+          </div>
+        )}
         <div className="form_block">
           계정이 이미 있으신가요?
           <Link to="/login" className="form_link">
@@ -30,7 +102,12 @@ export default function SignupForm() {
           </Link>
         </div>
         <div className="form_block">
-          <input type="submit" value="회원가입" className="form_btn-submit" />
+          <input
+            type="submit"
+            value="회원가입"
+            className="form_btn-submit"
+            disabled={error?.length > 0}
+          />
         </div>
       </form>
     </>
