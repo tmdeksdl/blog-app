8253bcec6565cf5a592b6e0cf20891250b780b49
jpark <tmdeksdl@gmail.com>
2023-12-29 16:44:44 +0900

    Refactor: 유저 정보 관리에 useContext 사용

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,32 @@
+import AuthContext from 'context/AuthContext';
 import { getAuth, signOut } from 'firebase/auth';
 import { app } from 'firebaseApp';
+import { useContext } from 'react';
 import { toast } from 'react-toastify';
 
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success('로그아웃 되었습니다.');
+  } catch (error: any) {
+    toast.error(error?.code);
+  }
+};
+
 export default function Profile() {
-  const auth = getAuth(app);
-  const onSignOut = async () => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      toast.success('로그아웃 되었습니다.');
-    } catch (error: any) {
-      toast.error(error?.code);
-    }
-  };
+  //useContext를 사용하기 때문에 아래 코드는 필요 없다.
+  // const auth = getAuth(app);
+
+  const { user } = useContext(AuthContext);
   return (
     <>
       <div className="profile_box">
         <div className="flex_box-lg">
           <div className="profile_image"></div>
           <div>
-            <div className="profile_email">{auth?.currentUser?.email}</div>
-            <div className="profile_name">
-              {auth?.currentUser?.displayName || '사용자'}
-            </div>
+            <div className="profile_email">{user?.email}</div>
+            <div className="profile_name">{user?.displayName || '사용자'}</div>
           </div>
         </div>
         <div role="presentation" className="profile_logout" onClick={onSignOut}>
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import firebase from 'firebaseApp';
+import { AuthContextProvider } from 'context/AuthContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </AuthContextProvider>
 );
