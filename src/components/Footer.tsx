9cce4b7ddf9b1d66d6501206bc2ff90487d99ade
jpark <tmdeksdl@gmail.com>
2024-01-01 03:25:07 +0900

    Feat: 다크모드 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,27 @@
 import { Link } from 'react-router-dom';
+import { BsMoonFill, BsSun } from 'react-icons/bs';
+import { ThemeContext } from 'context/ThemeContext';
+import { useContext } from 'react';
 
 export default function Header() {
+  const context = useContext(ThemeContext);
+  console.log(context);
+
   return (
-    <footer>
+    <footer className="footer">
       <Link to="/posts/new">글쓰기</Link>
       <Link to="/posts">게시글</Link>
       <Link to="/profile">프로필</Link>
+      <div>
+        {context.theme === 'light' ? (
+          <BsSun className="footer_theme-btn" onClick={context.toggleMode} />
+        ) : (
+          <BsMoonFill
+            className="footer_theme-btn"
+            onClick={context.toggleMode}
+          />
+        )}
+      </div>
     </footer>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import firebase from 'firebaseApp';
 import { AuthContextProvider } from 'context/AuthContext';
+import { ThemeContextProvider } from 'context/ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthContextProvider>
+  <ThemeContextProvider>
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  </ThemeContextProvider>
 );
